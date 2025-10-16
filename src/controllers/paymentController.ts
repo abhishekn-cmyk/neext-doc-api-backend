b33f1782a15974ebee157f46d70a3a20820a7232
Abhishek N <abhishekpes123@gmail.com>
2025-10-16 22:32:55 +0530

    first-commit

--- src/controllers/paymentController.ts
+++ src/controllers/paymentController.ts
@@ -2,7 +2,9 @@ import asyncHandler from "express-async-handler";
 import Stripe from "stripe";
 import dotenv from "dotenv";
 dotenv.config();
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
+  apiVersion: "2025-08-27.basil",
+});
 
 export const createPaymentIntent = asyncHandler(async (req, res) => {
   const session = await stripe.checkout.sessions.create({
